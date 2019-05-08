592b71e93b26dbf2a629c992e87cc120004914bd
Nabeel Farooqui <nabeelfarooqui98@yahoo.com>
2019-05-08 13:15:05 +0500

    on our images

--- SSIM.py
+++ SSIM.py
@@ -1,23 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import cv2 as cv
 from skimage import data, img_as_float
 from skimage.measure import compare_ssim as ssim
 
 
-img = img_as_float(data.camera())
+#img = img_as_float(data.camera())
+img = img_as_float(cv.imread('nabeel.jpg',0)) 
 rows, cols = img.shape
 
-noise = np.ones_like(img) * 0.2 * (img.max() - img.min())
-noise[np.random.random(size=noise.shape) > 0.5] *= -1
+
 
 def mse(x, y):
     return np.linalg.norm(x - y)
 
-img_noise = img + noise
-img_const = img + abs(noise)
+img_noise = img_as_float(cv.imread('nabeel_same_size.jpg',0))
 
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4),
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4),
                          sharex=True, sharey=True)
 ax = axes.ravel()
 
@@ -27,10 +27,6 @@ ssim_none = ssim(img, img, data_range=img.max() - img.min())
 mse_noise = mse(img, img_noise)
 ssim_noise = ssim(img, img_noise,
                   data_range=img_noise.max() - img_noise.min())
-
-mse_const = mse(img, img_const)
-ssim_const = ssim(img, img_const,
-                  data_range=img_const.max() - img_const.min())
 
 label = 'MSE: {:.2f}, SSIM: {:.2f}'
 
@@ -42,9 +38,6 @@ ax[1].imshow(img_noise, cmap=plt.cm.gray, vmin=0, vmax=1)
 ax[1].set_xlabel(label.format(mse_noise, ssim_noise))
 ax[1].set_title('Image with noise')
 
-ax[2].imshow(img_const, cmap=plt.cm.gray, vmin=0, vmax=1)
-ax[2].set_xlabel(label.format(mse_const, ssim_const))
-ax[2].set_title('Image plus constant')
 
 plt.tight_layout()
 #plt.show()
